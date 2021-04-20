86d5b1b6ada17b1c54149e0f736bd5d99b6ba145
amosmoyo <amosmoyo5300@gmail.com>
2021-04-20 14:27:58 +0300

    made some changes to the home page

--- src/app/main/usersdetails/signup-users/signup-users.component.ts
+++ src/app/main/usersdetails/signup-users/signup-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-users',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupUsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  save() {
+
+  }
+
+  organizationSignup() {
+    this.router.navigate(['organisation'], { relativeTo: this.route})
   }
 
 }
--- src/app/main/usersdetails/users/users.module.ts
+++ src/app/main/usersdetails/users/users.module.ts
@@ -3,17 +3,47 @@ import { CommonModule } from '@angular/common';
 import { LoginUsersComponent } from '../login-users/login-users.component';
 import { SignupUsersComponent } from '../signup-users/signup-users.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SignupCompanyComponent } from '../signup-users/company/signup-company/signup-company.component';
+import { SignupNormalusersComponent } from '../signup-users/normal-user/signup-normalusers/signup-normalusers.component';
+import { MaterialModule } from 'src/app/material.module';
+import { UsigninComponent } from '../login-users/signin/usignin/usignin.component';
+import { CsigninComponent } from '../login-users/signin/csignin/csignin.component';
+import { ValidateEqualModule } from 'ng-validate-equal';
 
 const routes: Routes = [
-  { path: 'login', component: LoginUsersComponent},
-  { path: 'signup', component: SignupUsersComponent},
+  { path: 'login', component: LoginUsersComponent,
+    children: [
+      {
+        path: 'user',
+        component: UsigninComponent
+      },
+      {
+        path: 'organization',
+        component: CsigninComponent
+      }
+    ]
+  },
+  { path: 'signup', component: SignupUsersComponent,
+   children: [
+     {
+       path:'user',
+       component: SignupNormalusersComponent
+     },
+     {
+       path: 'organization',
+       component: SignupCompanyComponent
+     }
+   ]
+  },
   { path: 'dashboard', loadChildren:()=>import('./../dashdetails/user-dashboard/user-dashboard.module').then(m => m.UserDashboardModule)}
 ]
 
 @NgModule({
-  declarations: [LoginUsersComponent, SignupUsersComponent],
+  declarations: [LoginUsersComponent, SignupUsersComponent, SignupNormalusersComponent, SignupCompanyComponent, CsigninComponent, UsigninComponent],
   imports: [
-    CommonModule, RouterModule.forChild(routes)
+    CommonModule, RouterModule.forChild(routes),
+    FormsModule,  MaterialModule, ValidateEqualModule
   ],
   exports: [RouterModule]
 })
